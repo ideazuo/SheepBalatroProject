336fdc17566d23869d5d785a88127a98ec4a1058
zuoqidi <idea2210@gmail.com>
2025-03-12 17:20:36 +0800

    Initial commit

---
+++ Scripts/GameScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour
+{
+    float dt;
+    private void Awake()
+    {
+        GameApp.Instance.Init();
+    }
+
+    void Start()
+    {
+        //播放背景音乐
+        GameApp.SoundManager.PlayBGM("StartBgm");
+    }
+
+    void Update()
+    {
+        dt = Time.deltaTime;
+        GameApp.Instance.Update(dt);
+    }
+}
