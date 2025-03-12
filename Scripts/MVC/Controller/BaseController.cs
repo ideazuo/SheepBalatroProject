fbc60322d623f6d81075c0cc333d73adfc552324
zuoqidi <idea2210@gmail.com>
2025-03-12 18:13:14 +0800

    增加Dotween插件和视图管理器

--- Scripts/MVC/Controller/BaseController.cs
+++ Scripts/MVC/Controller/BaseController.cs
@@ -11,6 +11,9 @@ public class BaseController
 
     protected BaseModel model;//模板数据
 
+    /// <summary>
+    /// 构造函数，初始化事件字典
+    /// </summary>
     public BaseController()
     {
         message = new Dictionary<string, System.Action<object[]>>();
@@ -22,9 +25,15 @@ public class BaseController
 
     }
 
-    public virtual void OnLoadView(IBaseView view) { }//加载视图
+    //视图相关方法
 
-    //打开视图
+    //加载视图
+    public virtual void OnLoadView(IBaseView view) { }
+
+    /// <summary>
+    /// 打开视图
+    /// </summary>
+    /// <param name="view">要打开的视图</param>
     public virtual void OpenView(IBaseView view)
     {
 
@@ -35,6 +44,8 @@ public class BaseController
     {
 
     }
+
+    //事件相关方法
 
     //注册模板事件
     public void RegisterFunc(string eventName, System.Action<object[]> callback)
@@ -74,36 +85,62 @@ public class BaseController
     //触发其它模板的事件
     public void ApplyControllerFunc(int controllerKey,string eventName, params object[] args)
     {
-        //Debug.Log($"ApplyControllerFunc called with controllerKey: {controllerKey}, eventName: {eventName}");
         GameApp.ControllerManager.ApplyFunc(controllerKey, eventName, args);
     }
 
+    /// <summary>
+    /// 触发其它控制器的事件（通过控制器类型）
+    /// </summary>
+    /// <param name="type">控制器类型</param>
+    /// <param name="eventName">事件名称</param>
+    /// <param name="args">事件参数</param>
     public void ApplyControllerFunc(ControllerType type, string eventName,params object[] args)
     {
         ApplyControllerFunc((int)type, eventName, args);
     }
 
-    //设置模型数据
+    //模型相关方法
+
+    /// <summary>
+    /// 设置模型数据并建立模型与控制器的关联
+    /// </summary>
+    /// <param name="model">要设置的模型</param>
     public void SetModel(BaseModel model)
     {
         this.model = model;
         this.model.controller = this;
     }
 
+    /// <summary>
+    /// 获取控制器关联的模型
+    /// </summary>
+    /// <returns>关联的模型实例</returns>
     public BaseModel GetModel()
     {
         return model;
     }
 
+    /// <summary>
+    /// 获取控制器关联的模型并转换为指定类型
+    /// </summary>
+    /// <typeparam name="T">目标模型类型</typeparam>
+    /// <returns>转换后的模型实例</returns>
     public T GetModel<T>() where T : BaseModel
     {
         return model as T;
     }
 
+    /// <summary>
+    /// 获取指定控制器的模型
+    /// </summary>
+    /// <param name="controllerKey">控制器的键值</param>
+    /// <returns>指定控制器的模型实例</returns>
     public BaseModel GetControllerModel(int controllerKey)
     {
         return GameApp.ControllerManager.GetControllerModel(controllerKey);
     }
+
+    //控制器相关方法
 
     //删除控制器
     public virtual void Destroy()
--- Scripts/MVC/ControllerManager.cs
+++ Scripts/MVC/ControllerManager.cs
@@ -8,11 +8,11 @@ using UnityEngine;
 /// </summary>
 public class ControllerManager
 {
-    private Dictionary<int, BaseController> _modules;//存储控制器的字典
+    private Dictionary<int, BaseController> _controllers;//存储控制器的字典
 
     public ControllerManager()
     {
-        _modules = new Dictionary<int, BaseController>();
+        _controllers = new Dictionary<int, BaseController>();
     }
 
     public void Register(ControllerType type,BaseController ctl)
@@ -23,16 +23,16 @@ public class ControllerManager
     //注册控制器
     public void Register(int controllerKey,BaseController ctl)
     {
-        if(_modules.ContainsKey(controllerKey) == false)
+        if(_controllers.ContainsKey(controllerKey) == false)
         {
-            _modules.Add(controllerKey, ctl);
+            _controllers.Add(controllerKey, ctl);
         }
     }
 
     //执行所有控制器Init函数
-    public void InitAllModules()
+    public void InitAllControllers()
     {
-        foreach(var item in _modules)
+        foreach(var item in _controllers)
         {
             item.Value.Init();
         }
@@ -41,44 +41,44 @@ public class ControllerManager
     //移除控制器
     public void UnRegister(int controllerKey)
     {
-        if(_modules.ContainsKey(controllerKey))
+        if(_controllers.ContainsKey(controllerKey))
         {
-            _modules.Remove(controllerKey);
+            _controllers.Remove(controllerKey);
         }
     }
 
     //清除
     public void Clear()
     {
-        _modules.Clear();
+        _controllers.Clear();
     }
 
     //清楚所有控制器
-    public void ClearAllModules()
+    public void ClearAllControllers()
     {
-        List<int> keys = _modules.Keys.ToList();
+        List<int> keys = _controllers.Keys.ToList();
         for(int i = 0; i <keys.Count; i++)
         {
-            _modules[keys[i]].Destroy();
-            _modules.Remove(keys[i]);
+            _controllers[keys[i]].Destroy();
+            _controllers.Remove(keys[i]);
         }
     }
 
     //跨模板触发消息
     public void ApplyFunc(int controllerKey, string eventName, System.Object[] args)
     {
-        if(_modules.ContainsKey(controllerKey))
+        if(_controllers.ContainsKey(controllerKey))
         {
-            _modules[controllerKey].ApplyFunc(eventName, args);
+            _controllers[controllerKey].ApplyFunc(eventName, args);
         }
     }
 
     //获取某控制器Model对象
     public BaseModel GetControllerModel(int controllerKey)
     {
-        if(_modules.ContainsKey(controllerKey))
+        if(_controllers.ContainsKey(controllerKey))
         {
-            return _modules[controllerKey].GetModel();
+            return _controllers[controllerKey].GetModel();
         }
         else
         {
