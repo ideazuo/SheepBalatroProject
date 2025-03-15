d8ba1d536b27ee2bbcc6d04f9ce5fbd8745f5e37
ideazuo <idea2210@gmail.com>
2025-03-15 14:27:17 +0800

    重构扑克牌手牌类型，将PokerHandType枚举移至新文件中以提高代码结构清晰度，同时在CardModel中移除冗余定义。

--- Scripts/Module/Card/CardModel.cs
+++ Scripts/Module/Card/CardModel.cs
@@ -117,25 +117,6 @@ public enum CardRank
 }
 
 /// <summary>
-/// 扑克牌手牌类型
-/// </summary>
-public enum PokerHandType
-{
-    HighCard,       // 高牌
-    OnePair,        // 对子
-    TwoPair,        // 两对
-    ThreeOfAKind,   // 三条
-    Straight,       // 顺子
-    Flush,          // 同花
-    FullHouse,      // 葫芦
-    FourOfAKind,    // 四条
-    StraightFlush,  // 同花顺
-    FiveOfAKind,    // 五条
-    FlushFullHouse, // 同花葫芦
-    FlushFiveOfAKind // 同花五条
-}
-
-/// <summary>
 /// 卡牌模型类，存储卡牌相关数据
 /// </summary>
 public class CardModel : BaseModel
